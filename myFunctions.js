aacac7b35f9fee80e376144228ed6909e7e7ffd8
Remi077 <77671776+Remi077@users.noreply.github.com>
2024-12-28 17:00:31 +0100

    Update myFunctions.js

--- myFunctions.js
+++ myFunctions.js
@@ -1,8 +1,97 @@
 // myFunctions.js
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.158.0/build/three.module.js';
+// import * as FBX from 'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/loaders/FBXLoader.js';
 import { FBXLoader } from './FBXLoader.js'
 
 //TEXT SPRITE
+
+export function initializeHubFromJson(jsonPath, thisDocument, thisCamera, thisScene) {
+    return fetch(jsonPath)
+        .then(response => response.json()) // Parse JSON
+        .then(jsonData => {
+            console.log('Loaded JSON HUB data:', jsonData);
+            // Use the data (which contains the URL, scale, type, etc.) 
+            // to load the resources and create objects.
+            // return all resources in a dictionary
+            const result = loadHUBResources(jsonData, thisDocument, thisCamera, thisScene);
+            console.log('Parsed JSON HUB data');
+            return result;
+        }).catch(error => {
+            console.error('Error loading JSON HUB:', error);
+            throw error; // Re-throw the error for upstream handling
+        });
+}
+
+export function loadHUBResources(jsonData, thisDocument, thisCamera, thisScene) {
+    const loadPromises = Object.entries(jsonData).map(([key, data]) => {
+        if (!data) {
+            console.warn(`Skipping entry with missing data for key: ${key}`);
+            return Promise.resolve([key, null]); // Return null for missing or invalid data
+        }
+        return initializeTextSprite(thisDocument, thisCamera, thisScene, data).then(sprite => {
+            return [key, sprite]; // Return the sprite dictionary
+        });
+    });
+    // Wait for all images to load and return the results
+    return Promise.all(loadPromises).then(results => {
+        return Object.fromEntries(results); // Convert back to a dictionary { key: sprite/texture }
+    });
+}
+
+function addEntries(originalDict, newEntries) {
+    return { ...originalDict, ...newEntries };
+}
+
+
+export function initializeTextSprite(thisDocument, thisCamera, thisScene, jsonData) {
+    const textCanvas = thisDocument.createElement('canvas');
+    const context = textCanvas.getContext('2d');
+    textCanvas.width = 256;
+    textCanvas.height = 128;
+
+    context.fillStyle = jsonData.color;
+    context.font = '30px Arial';
+    context.textAlign = 'center';
+    context.fillText(jsonData.text, textCanvas.width / 2, textCanvas.height / 2);
+
+    const texture = new THREE.CanvasTexture(textCanvas);
+    const material = new THREE.SpriteMaterial({
+        map: texture,
+        depthTest: false // Disable depth test (never occluded)
+    });
+    let mycolor;
+    let redcolor = new THREE.Color(1, 0, 0);
+    let greencolor = new THREE.Color(0, 1, 0);
+    let bluecolor = new THREE.Color(0, 0, 1);
+    if (jsonData.color == "red") mycolor = redcolor;
+    else mycolor = greencolor;
+    const nMaterial = new THREE.SpriteMaterial({
+        color: mycolor,
+        depthTest: false // Disable depth test (never occluded)
+    });
+    const sprite = new THREE.Sprite(nMaterial);
+
+    //add to scene
+    thisScene.add(sprite);
+
+    // sprite.position.set(position.x, position.y, position.z);
+    if (!jsonData.scale) console.warn("scale not defined for sprite");
+    sprite.scale.set(1 * jsonData.scale, 0.5 * jsonData.scale, 1);  // Adjust the scale as needed
+
+    const offset = alignHUBToCamera(sprite, thisCamera, jsonData.alignX, jsonData.alignY, jsonData.padX, jsonData.padY);
+
+    const result =
+        addEntries(jsonData,
+            {
+                SPRITE: sprite,
+                CANVAS: textCanvas,
+                CONTEXT: context,
+                TEXTURE: texture,
+                OFFSET: offset,
+            }
+        )
+    return Promise.resolve(result);
+}
 
 export function alignHUBToCamera(sprite, thisCamera, alignX = 'center', alignY = 'middle', padx = 0, pady = 0) {
 
@@ -15,13 +104,19 @@ export function alignHUBToCamera(sprite, thisCamera, alignX = 'center', alignY =
         case "center": posX = 0; break;
         case "left": posX = -1 + padx; break;
         case "right": posX = 1 - padx; break;
-        default: posX = 0;
+        default: {
+            // console.error(alignX, "not supported");
+            posX = 0;
+        }
     }
     switch (alignY) {
         case "middle": posY = 0; break;
         case "bottom": posY = -1 + pady; break;
         case "top": posY = 1 - pady; break;
-        default: posY = 0;
+        default: {
+            // console.error(alignY, "not supported");
+            posY = 0;
+        }
     }
 
     const vector = new THREE.Vector3(
@@ -29,46 +124,37 @@ export function alignHUBToCamera(sprite, thisCamera, alignX = 'center', alignY =
         posY,
         -1 // Depth in NDC (-1 is near the camera)
     );
-
+    const oldVector = [...vector];
     // Unproject the screen position to world coordinates
+    thisCamera.updateProjectionMatrix();
     vector.unproject(thisCamera);
-
+console.log("cam",thisCamera.position, thisCamera.rotation, thisCamera.projectionMatrix,oldVector,vector );
     // Ensure the sprite stays in view
     sprite.position.copy(vector);
 
     // Calculate the offset vector 
-    var offsetVector = new THREE.Vector3().subVectors(sprite.position, thisCamera.position);
+    const offsetVector = new THREE.Vector3().subVectors(sprite.position, thisCamera.position);
     return (offsetVector);//return offset
 }
 
-export function initializeTextSprite(thisDocument, text, thisCamera, scale = 10, color = 'black', alignX, alignY, padX, padY) {
-    const textCanvas = thisDocument.createElement('canvas');
-    const context = textCanvas.getContext('2d');
-    textCanvas.width = 256;
-    textCanvas.height = 128;
+// updateHUD function
 
-    context.fillStyle = color;
-    context.font = '30px Arial';
-    context.textAlign = 'center';
-    context.fillText(text, textCanvas.width / 2, textCanvas.height / 2);
+function updateHUD() {
 
-    const texture = new THREE.CanvasTexture(textCanvas);
-    const material = new THREE.SpriteMaterial({
-        map: texture,
-        depthTest: false // Disable depth test (never occluded)
-    });
-    const sprite = new THREE.Sprite(material);
-    // sprite.position.set(position.x, position.y, position.z);
-    sprite.scale.set(1 * scale, 0.5 * scale, 1);  // Adjust the scale as needed
+    if (frameCount < 3) {
+        for (const [key, sprite] of Object.entries(HUBDict)) {
+            let noffset = alignHUBToCamera(sprite.SPRITE, camera, sprite.alignX, sprite.alignY, sprite.padX, sprite.padY);
+            console.log(key, `noffset`, noffset, sprite.OFFSET);
+        };
+    } else {
 
-    alignHUBToCamera(sprite, thisCamera, alignX, alignY, padX, padY);
-
-    sprite.textCanvas = textCanvas;
-    sprite.context = context;
-    sprite.texture = texture;
-
-    return sprite;
+        for (const [key, sprite] of Object.entries(HUBDict)) {
+            // console.log(`${key}: ${sprite}`);
+            sprite.SPRITE.position.copy(camera.position).add(sprite.OFFSET);
+        };
+    }
 }
+
 
 /* createSprite */
 
@@ -108,17 +194,6 @@ export function createMaterial(image, transparent = false, wrapX = 1, wrapY = 1)
     return imageMaterial;
 }
 
-/* loadImage */
-
-export function loadImage(src) {
-    return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.crossOrigin = 'Anonymous';
-        img.src = src;
-        img.onload = () => resolve(img); // Resolve promise when image is loaded
-        img.onerror = () => reject(new Error(`Failed to load image: ${src}`)); // Reject promise if there's an error
-    });
-}
 
 /* updateAnimations */
 export function updateAnimations(thisMixer, delta) {
@@ -187,6 +262,18 @@ export function loadImages(jsonData) {
     // Wait for all images to load and return the results
     return Promise.all(loadPromises).then(results => {
         return Object.fromEntries(results); // Convert back to a dictionary { key: sprite/texture }
+    });
+}
+
+/* loadImage */
+
+export function loadImage(src) {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.crossOrigin = 'Anonymous';
+        img.src = src;
+        img.onload = () => resolve(img); // Resolve promise when image is loaded
+        img.onerror = () => reject(new Error(`Failed to load image: ${src}`)); // Reject promise if there's an error
     });
 }
 
@@ -305,9 +392,11 @@ export function loadAnimation(loader, mixer, src) {
     })
 }
 
-export function updateTextSprite(sprite, newText, color = 'Red') {
-    const { textCanvas, context, texture } = sprite;
-    console.log("textCanvas, context, texture", textCanvas, context, texture);
+export function updateTextSprite(spriteData, newText, color = 'Red') {
+    const textCanvas = spriteData.CANVAS;
+    const context = spriteData.CONTEXT;
+    const texture = spriteData.TEXTURE;
+    // console.log("textCanvas, context, texture", textCanvas, context, texture);
 
     // Clear the canvas
     context.clearRect(0, 0, textCanvas.width, textCanvas.height);
